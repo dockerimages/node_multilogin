c9c67219b32f730bef9291f2d2dd0f53c8955749
Frank Lemanschik <frank.l@dspeed.de>
2014-06-13 19:18:28 +0200

    Init Commit Updated easy_node_authencication to Express 4 Framework

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://dspeed_accounts_mongodb:27017/interspeed' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
