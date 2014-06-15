85eafda9c7e220b064eb2214743a75b43b770035
Frank Lemanschik <frank@dspeed.eu>
2014-06-15 19:55:49 +0200

    Update database.js

--- easy_node_authentication/config/database.js
+++ easy_node_authentication/config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://dspeed_accounts_mongodb:27017/interspeed' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'readurl' : 'mongodb://dspeed_accounts_mongodb:27017/interspeed' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'writeurl' : 'mongodb://dspeed_accounts_mongodb:27017/interspeed' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
